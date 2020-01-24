984c4d032dbc85257ace4bcaed80ed786d0b3be2
Khichi-Hardik <hardikkhichi@gmail.com>
2020-01-24 18:01:04 +0530

    ADD & CHANGE: sequential using arrays

--- A1/a1_pthreads.cpp
+++ A1/a1_pthreads.cpp
@@ -30,6 +30,12 @@ double** multiply_matrix(double **a, double **b){
     return mult;
 }
 
+struct thread_args{
+    int t; // number of threads
+    int n;
+    int thread_tid;
+};
+
 int main(int argc, char *argv[]){
     n = stoi(argv[1]);
     t = stoi(argv[2]);
@@ -56,6 +62,8 @@ int main(int argc, char *argv[]){
 
     int* pi = new int[n];
 
+    pthread_t tids[t];
+
     srand(1); // Send the same seed if you want to parallelize this part
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -69,51 +77,51 @@ int main(int argc, char *argv[]){
         pi[i] = i;
     }
 
-    for(int k = 0 ; k < n ; k +=1){
-        double max = 0.0;
-        int kPrime ;
-        for(int i = k; i < n ; i+= 1){
-            if(max < abs(a[i][k])){
-                max = a[i][k] ;
-                kPrime = i ;
-            }
-        }
-        if(max == 0){
-            cout << "error : singular matrix" << endl ;
-            exit(0);
-        }
-        int tempInt;
-        tempInt = pi[k];
-        pi[k] = pi[kPrime];
-        pi[kPrime] = tempInt;
+    // for(int k = 0 ; k < n ; k +=1){
+    //     double max = 0.0;
+    //     int kPrime ;
+    //     for(int i = k; i < n ; i+= 1){
+    //         if(max < abs(a[i][k])){
+    //             max = a[i][k] ;
+    //             kPrime = i ;
+    //         }
+    //     }
+    //     if(max == 0){
+    //         cout << "error : singular matrix" << endl ;
+    //         exit(0);
+    //     }
+    //     int tempInt;
+    //     tempInt = pi[k];
+    //     pi[k] = pi[kPrime];
+    //     pi[kPrime] = tempInt;
         
-        double tempDouble;
-        for(int co = 0 ; co < n ; co +=1){
-            tempDouble = a[k][co];
-            a[k][co] = a[kPrime][co];
-            a[kPrime][co] = tempDouble;
-        }
-        for(int co = 0 ; co < k ; co+=1){
-            tempDouble = l[k][co];
-            l[k][co] = l[kPrime][co];
-            l[kPrime][co] = tempDouble;
-        }
-        u[k][k] = a[k][k];
-        for(int i = k+1 ; i < n ; i+=1){
-            l[i][k] = a[i][k] / u[k][k];
-            u[k][i] = a[k][i];
-        }
-        for(int i = k+1 ; i < n ; i+=1){
-            for(int j = k+1 ; j < n ; j+=1){
-                a[i][j] = a[i][j] - l[i][k]*u[k][j];
-            }
-        }
-    }
+    //     double tempDouble;
+    //     for(int co = 0 ; co < n ; co +=1){
+    //         tempDouble = a[k][co];
+    //         a[k][co] = a[kPrime][co];
+    //         a[kPrime][co] = tempDouble;
+    //     }
+    //     for(int co = 0 ; co < k ; co+=1){
+    //         tempDouble = l[k][co];
+    //         l[k][co] = l[kPrime][co];
+    //         l[kPrime][co] = tempDouble;
+    //     }
+    //     u[k][k] = a[k][k];
+    //     for(int i = k+1 ; i < n ; i+=1){
+    //         l[i][k] = a[i][k] / u[k][k];
+    //         u[k][i] = a[k][i];
+    //     }
+    //     for(int i = k+1 ; i < n ; i+=1){
+    //         for(int j = k+1 ; j < n ; j+=1){
+    //             a[i][j] = a[i][j] - l[i][k]*u[k][j];
+    //         }
+    //     }
+    // }
 
-    for(int i = 0; i < n; i++){
-        int pos = pi[i];
-        p[i][pos] = 1;
-    }
+    // for(int i = 0; i < n; i++){
+    //     int pos = pi[i];
+    //     p[i][pos] = 1;
+    // }
 
 
     // cout << endl << "a" << endl;
--- A1/run.sh
+++ A1/run.sh
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
-g++ a1_openmp.cpp
-./a.out $1 $2
-=======
-rm mat.txt
-g++ generateMat.cpp
-./a.out 1000 > mat.txt
-g++ a1_att2.cpp
-./a.out mat.txt 
->>>>>>> a2aebee0cf314e038871bb86e61079383448e99b
+# rm mat.txt
+# g++ generateMat.cpp
+# ./a.out 1000 > mat.txt
+# g++ a1_pthreads.cpp
+# time ./a.out 7000 2
+rm ./a.out
+g++ -lpthread a1_pthreads.cpp
+time ./a.out 150 2
